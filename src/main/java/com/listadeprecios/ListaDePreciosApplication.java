5f1b7f340a07951d1d8935f10aeccf1572f02a9a
Axcel Espinoza <aespinoza@procamaronex.com>
2022-10-20 14:38:59 -0500

    Initial commit

---
+++ src/main/java/com/listadeprecios/ListaDePreciosApplication.java
@@ -0,0 +1,13 @@
+package com.listadeprecios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDePreciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaDePreciosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/listadeprecios/ListaDePreciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listadeprecios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDePreciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
